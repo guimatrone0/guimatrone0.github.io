6b6e49e6dcb4acf9359ea4266bb04b30b59f1f4d
guimatrone0 <ggmatrone@gmail.com>
2024-06-01 19:28:08 -0300

    Create processa_contato.php

---
+++ processa_contato.php
@@ -0,0 +1,20 @@
+<?php
+// processa_contato.php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = htmlspecialchars($_POST['nome']);
+    $email = htmlspecialchars($_POST['email']);
+    $mensagem = htmlspecialchars($_POST['mensagem']);
+
+    // Exemplo de envio de email (simples)
+    $to = "seuemail@example.com";
+    $subject = "Nova mensagem de contato";
+    $body = "Nome: $nome\nEmail: $email\n\nMensagem:\n$mensagem";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Erro ao enviar mensagem.";
+    }
+}
+?>
